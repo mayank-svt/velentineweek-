1db075ce2afb66ca23cd974d4f64ef3e11315e6b
Mayank Shrivastava <mshrivastava8894@gmail.com>
2026-02-07 21:56:30 +0530

    Update script.js
    For propose day

--- script.js
+++ script.js
@@ -1,40 +1,29 @@
-let noCount = 0;
+const heartsContainer = document.querySelector(".hearts");
+const envelope = document.getElementById("envelope");
+const letterBox = document.getElementById("letterBox");
 
-const noBtn = document.getElementById("noBtn");
-const yesBtn = document.getElementById("yesBtn");
-const emotionText = document.getElementById("emotionText");
-const message = document.getElementById("message");
+/* HEART RAIN */
+function createHeart() {
+  const heart = document.createElement("span");
+  heart.innerText = "💗";
+  heart.style.left = Math.random() * 100 + "vw";
+  heart.style.animationDuration = Math.random() * 3 + 4 + "s";
+  heartsContainer.appendChild(heart);
 
-noBtn.addEventListener("mouseover", moveNo);
-noBtn.addEventListener("touchstart", moveNo);
-
-yesBtn.addEventListener("click", yesClicked);
-
-function moveNo() {
-  noCount++;
-
-  if (noCount >= 3 && noCount < 6) {
-    emotionText.innerText = "Pleaseeee 🥺💗";
-    emotionText.style.display = "block";
-  }
-
-  if (noCount >= 6) {
-    emotionText.innerText = "Okay… last chance 😭💔";
-    emotionText.style.display = "block";
-  }
-
-  const card = document.querySelector(".card");
-  const maxX = card.clientWidth - noBtn.clientWidth;
-  const maxY = 120;
-
-  noBtn.style.left = Math.random() * maxX + "px";
-  noBtn.style.top = Math.random() * maxY + "px";
-  noBtn.style.transform = "none";
+  setTimeout(() => {
+    heart.remove();
+  }, 7000);
 }
 
-function yesClicked() {
-  document.body.classList.add("rainbow");
-  message.style.display = "block";
+setInterval(createHeart, 500);
+
+/* OPEN LETTER */
+const envelopeContainer = document.getElementById("envelopeContainer");
+
+envelope.addEventListener("click", () => {
+  envelopeContainer.style.display = "none";
+  letterBox.style.display = "flex";
+});  message.style.display = "block";
   emotionText.style.display = "none";
   startHearts();
 }
